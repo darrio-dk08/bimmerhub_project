5dcb497b07e0f19b60719536f616967480e8a8d8
DK <darrio.dk@gmail.com>
2026-07-09 20:32:16 +0100

    Create parts and garage database models

--- garage/models.py
+++ garage/models.py
@@ -1,17 +1,33 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 
 class Vehicle(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    make = models.CharField(max_length=100, default="BMW")
+    """
+    Stores vehicles belonging to users.
+    """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="vehicles"
+    )
+
+    make = models.CharField(
+        max_length=50,
+        default="BMW"
+    )
+
     model = models.CharField(max_length=100)
+
     year = models.PositiveIntegerField()
+
     engine = models.CharField(max_length=100)
-    registration = models.CharField(max_length=20, blank=True)
+
+    registration = models.CharField(
+        max_length=20,
+        blank=True
+    )
 
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
