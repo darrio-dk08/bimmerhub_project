a931b3ff45a13c735f834df5a54e00d1b37776d1
DK <darrio.dk@gmail.com>
2026-07-25 14:40:35 +0100

    Configure media URLs for product images

--- bimmerhub/settings.py
+++ bimmerhub/settings.py
@@ -143,3 +143,6 @@ ACCOUNT_EMAIL_REQUIRED = False
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
